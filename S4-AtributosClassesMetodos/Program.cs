291bc1ad57fd7bd7c27d762501a96932b63d2081
Marlon James <marlonjamesdev@gmail.com>
2023-12-17 21:18:33 -0300

    Content added.

---
+++ S4-AtributosClassesMetodos/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using S4_AtributosClassesMetodos;
+
+namespace S4_ClassesAtributosMetodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProblemaExemplo(); // Linha 21.
+            Classe(); // Linha 59.
+
+            Console.ReadLine();
+        }
+
+        // Resolveremos um problema primeiro sem usar orientação à objetos e logo resolveremos o mesmo problema com a orientação à objetos.
+        static void ProblemaExemplo()
+        {
+            /*
+            Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas válidas). 
+            Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos possui a maior área.
+            */
+
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+
+        // Aplicando orientação à objetos para resolver o mesmo problema.
+        static void Classe()
+        {
+            /*
+            É um tipo estruturado que pode conter membros:
+            - Atributos (dados / Campos)
+            - Métodos (funções / operações)
+
+            A classe também pode prover muitos outros recursos, tais como:
+            - Construtores
+            - Sobrecarga
+            - Encapsulamento
+            - Herança
+            - Polimorfismo
+
+            Exemplos:
+            - Entidades: Produto, Cliente, Triângulo
+            - Serviçoes: ProdutoService, ClienteService, EmailService, StorageService
+            - Controladores: ProdutoController, ClienteController
+            - Utilitários: Calculadora, Compactador
+            - Outros (views, repositórios, gerenciadores, etc.)
+
+            Por padrão, nomes de classes e atributos devem começar com letra maiúscula.
+            */
+
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        }
+
+    }
+}
---
+++ S4-AtributosClassesMetodos/Triangulo.cs
@@ -0,0 +1,9 @@
+﻿namespace S4_AtributosClassesMetodos
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
