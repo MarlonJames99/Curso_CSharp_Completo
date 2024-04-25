5c38612e17818b5140bc9c81e8ddc669531be0a0
Marlon James <marlonjamesdev@gmail.com>
2024-04-24 22:36:09 -0300

    Content added.

--- S7-TopicosEspeciaisEmCSharpPT1/Program.cs
+++ S7-TopicosEspeciaisEmCSharpPT1/Program.cs
@@ -15,6 +15,8 @@ namespace S7_TopicosEspeciaisEmCSharpPT1
             Var(); // Linha .
             SwitchCase(); // Linha .
             ExpressaoCondicionalTernaria(); // Linha .
+            FuncoesInteressantesStrings(); // Linha .
+            FuncaoDateTime(); // Linha .
         }
 
         static void Var()
@@ -152,6 +154,116 @@ namespace S7_TopicosEspeciaisEmCSharpPT1
 
             double desc = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
             Console.WriteLine(desc);
+        }
+
+        static void FuncoesInteressantesStrings()
+        {
+            // Funções que podemos utilizar com strings.
+
+            string original = "abcde FGHIJ ABC abc DEFG     ";
+
+            string s1 = original.ToUpper(); // Converte toda a string para maíusculas.
+            string s2 = original.ToLower(); // Converte toda a string para minúsculas.
+            string s3 = original.Trim(); // Apaga espaços vazios tanto antes quanto depois da string.
+
+            int n1 = original.IndexOf("bc"); // Busca o index de onde inicia a primeira ocorrência do trecho citado dentro da string em questão.
+            int n2 = original.LastIndexOf("bc"); // Busca o index de onde inicia a última ocorrência do trecho citado dentro da string em questão.
+
+            string s4 = original.Substring(3); // Recorta a string a partir do index informado, nesse caso, a partir do index 3.
+                                               // Também podemos informar o tamanho que queremos que ele recorte.
+
+            string s5 = original.Substring(3, 5); // Recorta a partir do index 3, apenas 5 elementos.
+
+            string s6 = original.Replace('a', 'x'); // Substitui o primeiro elemento pelo segundo, nesse caso, todo caracter 'a' por 'x'.
+            string s7 = original.Replace("abc", "xy"); // Mostra que o substituto não precisa necessariamente ter o mesmo tamanho.
+
+            bool b1 = string.IsNullOrEmpty(original); // Testa se o conteúdo é vazio ou nulo.
+            bool b2 = string.IsNullOrWhiteSpace(original); // Testa se o conteúdo é nulo ou vários espaços em branco.
+
+            Console.WriteLine("Original: - " + original + "-");
+            Console.WriteLine("ToUpper: - " + s1 + "-");
+            Console.WriteLine("ToLower: - " + s2 + "-");
+            Console.WriteLine("Trim: - " + s3 + "-");
+            Console.WriteLine("IndexOf ('bc'): - " + n1 + "-");
+            Console.WriteLine("LastIndexOf ('bc'): - " + n2 + "-");
+            Console.WriteLine("Substring(3): -" + s4 + "-");
+            Console.WriteLine("Substring(3, 5): -" + s5 + "-");
+            Console.WriteLine("Replace('a', 'x'): -" + s6 + "-");
+            Console.WriteLine("Replace('abc', 'xy'): -" + s7 + "-");
+            Console.WriteLine("IsNullOrEmpty: " + b1);
+            Console.WriteLine("IsNullOrWhiteSpace: " + b2);
+
+            /*
+            Além das mencionadas acima, temos outras que já vimos no decorrer do curso, como:
+
+            - str.Split
+            - int x = int.Parse(str)
+            - str = x.ToString("F2")
+            */
+        }
+
+        static void FuncaoDateTime()
+        {
+            /*
+            Representa um instante.
+            É um tipo valor (struct).
+
+            Representação Interna
+            - Um objeto DateTime internamente armazena:
+              * O número de "ticks" (100 nanosegundos) desde a meia noite do dia 1 de janeiro do ano 1 da era comum.
+            */
+
+            DateTime d1 = DateTime.Now; // Pega o horário atual do sistema.
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks); // Quantidade de ticks que passaram até o momento atual.
+
+            /*
+            Instanciação
+
+            Construtores:
+            - DateTime(ano, mes, dia)
+            - DateTime(ano, mes, dia, hora, minuto, segundo)
+            - DateTime(ano, mes, dia, hora, minuto, segundo, milissegundo)
+
+
+            Builders:
+            - DateTime.Now
+            - DateTime.UtcNow
+            - DateTime.Today
+            - DateTime.Parse(string)
+            - DateTime.ParseExact(string, string)
+            */
+
+            DateTime d2 = new DateTime(2018, 11, 25); // Instanciamos um DateTime do dia 25/11/2018 às 00:00.
+            DateTime d3 = new DateTime(2018, 11, 25, 20, 45, 03); // Instanciamos um DateTime do dia 25/11/2018 às 20:45:03.
+            DateTime d4 = new DateTime(2018, 11, 25, 20, 45, 03, 500); // Instanciamos um DateTime do dia 25/11/2018 às 20:45:03 (os milissegundos não são mostrados na formatação padrão).
+
+            DateTime d5 = DateTime.Now; // Horário atual do sistema.
+            DateTime d6 = DateTime.Today; // Data de hoje às 00:00.
+            DateTime d7 = DateTime.UtcNow; // Igual ao .Now porém pegando o horário padrão GMT (de Greenwich).
+
+            DateTime d8 = DateTime.Parse("2000-08-15"); // Converte uma string para data.
+            DateTime d9 = DateTime.Parse("2000-08-15 13:05:58"); // Converte uma string para data.
+
+            DateTime d10 = DateTime.Parse("15-08-2000"); // No formato do Brasil.
+            DateTime d11 = DateTime.Parse("15-08-2000 13:05:58"); // No formato do Brasil.
+
+            DateTime d12 = DateTime.ParseExact("2000-08-15", "yyyy-mm-dd", CultureInfo.InvariantCulture); // Este nos permite escolher qual formato exatamente nós queremos utilizar.
+            DateTime d13 = DateTime.ParseExact("15/08/2000 13:05:58", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture); // Testando em outro formato.
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine(d7);
+            Console.WriteLine(d8);
+            Console.WriteLine(d9);
+            Console.WriteLine(d10);
+            Console.WriteLine(d11);
+            Console.WriteLine(d12);
+            Console.WriteLine(d13);
         }
     }
 }
