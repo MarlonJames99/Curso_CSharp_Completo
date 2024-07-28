79d87792f8637ec5ec1a0e79316734123763ff82
Marlon James <marlonjamesdev@gmail.com>
2024-07-28 18:02:28 -0300

    Content Added.

---
+++ S10-HerancaEPolimorfismo/Entities/SavingsAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10_HerancaEPolimorfismo.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
--- S10-HerancaEPolimorfismo/Program.cs
+++ S10-HerancaEPolimorfismo/Program.cs
@@ -63,6 +63,39 @@ namespace S10_HerancaEPolimorfismo
               - Palavra is
               - Uso comum: Métodos que recebem parâmetros genéricos (ex: Equals)
             */
+
+            Account acc = new Account(1001, "Alex", 0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            // UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Ana", 0.0, 0.01);
+
+            // DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            // BusinessAccount acc5 = (BusinessAccount)acc3; // Aqui, apesar do compilador não detectar um erro,
+                                                          // ao executar o código, dará um erro por estar convertendo um conteúdo do tipo "Savings Account" para "Business Account".
+
+            // Testando previamente se o conteúdo é ou não do tipo que você deseja:
+
+            if(acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if(acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = acc3 as SavingsAccount; // Sintaxe alternativa de fazer o Downcasting, usando "as".
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
         }
     }
 }
